98e929674de51d5a6fa50a3f0a9cd47efa9169a4
Ahere George Odongo <aheregeorge@hotmail.co.uk>
2016-06-03 18:39:22 +0800

    Alot of changes after longish break

---
+++ Assets/Scripts/PolyCount.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PolyCount : MonoBehaviour {
+
+    public Text triangleCountText;
+    public int numTriangles;
+
+	// Use this for initialization
+	void Start () 
+	{   
+        if (gameObject.GetComponent<SkinnedMeshRenderer>())
+        {
+			numTriangles = gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh.triangles.Length/3;
+	        Debug.Log(numTriangles);
+	        triangleCountText.text = "PolyCount = " + numTriangles.ToString();
+        }
+
+        else
+        {
+        	 numTriangles = gameObject.GetComponent<MeshFilter>().mesh.triangles.Length/3;
+             Debug.Log(numTriangles);
+             triangleCountText.text ="PolyCount = " + numTriangles.ToString();
+
+        }
+       
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
